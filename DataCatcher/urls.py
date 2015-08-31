88f82579d75f7d8037f1610c0cc52a9d0ebf5d34
Chris Hardee <shazzner@gmail.com>
2015-08-31 13:54:25 -0500

    Added home app, starting the visual design

--- DataCatcher/urls.py
+++ DataCatcher/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('home.urls', namespace='home')),
     url(r'^weather/', include('weather.urls', namespace='weather')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
 
 from .models import DataSlice
 
