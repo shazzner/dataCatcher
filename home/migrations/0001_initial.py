c27efe4f2cf62d56ad605501b0a236e313fa84f0
Chris Hardee <shazzner@gmail.com>
2015-08-31 21:48:46 -0500

    Added in Page model, more view/css adjustments

---
+++ home/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Page',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('page_title', models.CharField(max_length=50)),
+                ('page_content', models.CharField(max_length=300)),
+            ],
+        ),
+    ]
--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    page_title = models.CharField(max_length=50)
+    page_content = models.CharField(max_length=300)
+
+    def __unicode__(self):
+        return self.page_title
