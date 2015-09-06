942dd2afa311f80a804d974905cfaef33aab7638
Chris Hardee <shazzner@gmail.com>
2015-09-06 03:36:26 -0500

    Added in urls and views grabbing datetime in url

--- weather/urls.py
+++ weather/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<data_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^data/(?P<styear>\d{4})-(?P<stmonth>\d{2})-(?P<stday>\d+)/(?P<edyear>\d{4})-(?P<edmonth>\d{2})-(?P<edday>\d+)/$', views.timeslice, name='timeslice'),
+
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-
+import datetime
 from .models import DataSlice
 
 # Create your views here.
@@ -11,3 +11,11 @@ def index(request):
 def detail(request, data_id):
     data_id = get_object_or_404(DataSlice, pk=data_id)
     return render(request, 'weather/detail.html', {'data_id': data_id})
+
+def timeslice(request, styear, stmonth, stday, edyear, edmonth, edday):
+    start_date = styear + '-' + stmonth + '-' + stday + ' 00:00'
+    start_slice = datetime.datetime.strptime(start_date, '%Y-%m-%d %H:%M')
+    end_date = edyear + '-' + edmonth + '-' + edday + ' 23:59'
+    end_slice = datetime.datetime.strptime(end_date, '%Y-%m-%d %H:%M')
+    range_slices = DataSlice.objects.filter(capture_time__range=(start_date, end_date))
+    return render(request, 'weather/timeslice.html', {'range_slices': range_slices})
