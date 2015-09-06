d115b59ed77fdf3db82f843a5b7b4e19b4a46ce9
Chris Hardee <shazzner@gmail.com>
2015-09-06 01:41:14 -0500

    Added static pages, cleaned up views

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^listings/', views.listings, name='listings'),
+    url(r'^about/', views.about, name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,6 @@ def listings(request):
 def index(request):
     return render(request, 'home/index.html')
 
+def about(request):
+    return render(request, 'home/about.html')
+
